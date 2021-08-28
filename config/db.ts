0adffb162bce1a0de48b54bb25ca2ec0fd0b997f
Aragor70 <mikey.prus@gmail.com>
2021-08-28 20:04:32 +0000

    Update server: Register user route.

--- config/db.ts
+++ config/db.ts
@@ -1,14 +1,18 @@
-import { Schema, model, createConnection } from 'mongoose';
+import { Schema, model, connect } from 'mongoose';
 
 
 const db: any = process.env.mongoPublicURI;
 
+const options: any = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
 
 export default async () => {
     try {
-        const conn: any = await createConnection(db, {});
+        const conn: any = await connect(db, options);
         console.log(`Ciao, MongoDB connected... `);
-        console.log(conn.client.options.srvHost)
+        console.log(conn.connection.host)
     } catch(err: any){
         console.error(err.message);
         process.exit(1);
--- routes/api/users.ts
+++ routes/api/users.ts
@@ -1,20 +1,61 @@
 import User from "../../models/User";
 
 import express, {Request, Response, Application, Router} from 'express';
-
+import gravatar from 'gravatar';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const router: Router = express.Router();
 
 
 router.post('/', async (req: Request, res: Response) => {
     
-    const { email, password } = req.body;
 
     try {
+        
+        const { name, email, password } = req.body;
 
-        console.log(email)
+        let user: any = await User.findOne(({ email }))
 
-        res.json(req.body)
+        if (user) {
+            return res.status(400).json({ errors: [ { msg: 'User already exists.' } ] })
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200', r: 'pg', d: 'mm'
+        //  size, rating, default image
+        });
+
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        // encrypt password using bcrypt
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        const JWTSecretKey: any = process.env["jwtSecret"]
+        jwt.sign(payload, JWTSecretKey, { expiresIn: 360000 },
+        (err, token) => {
+            if(err) {
+                throw err
+            }
+            res.json({ token }); 
+                
+        });
+        
 
     } catch(err: any){
         console.error(err.message);
