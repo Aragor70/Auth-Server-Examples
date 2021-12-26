45de3615f088aa0269610c7f026a56e5d8904ffa
Mikolaj Prus <mikis3@o2.pl>
2021-12-26 21:44:50 +0000

    Server update: Implement controllers

--- routes/api/auth.ts
+++ routes/api/auth.ts
@@ -1,48 +1,14 @@
-import User from "../../models/User";
 
-import express, {Request, Response, Router, NextFunction} from 'express';
 
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import ErrorResponse from "../../utils/ErrorResponse";
-import asyncHandler from "../../middlewares/async";
+import express, { Router } from 'express';
 
 
 const router: Router = express.Router();
 
+import AuthControlller from '../../controllers/AuthController';
 
-router.post('/', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    
-    const { email, password } = req.body;
+const authControlller = new AuthControlller;
 
-    let user = await User.findOne(({ email }));
-
-    if(!user){
-        return next(new ErrorResponse('Invalid Credentials.', 422))
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch){
-        return next(new ErrorResponse('Invalid Credentials.', 422))
-    }
-
-    const payload = {
-        user: {
-            id: user.id
-        }
-    }
-    const JWTSecretKey: any = process.env["jwtSecret"]
-    return jwt.sign(payload, JWTSecretKey, { expiresIn: 360000 },
-        (err, token) => {
-            if(err) {
-                return next(new ErrorResponse(err.message, 422))
-            }
-            res.json({ success: true, token }); 
-            
-    });
-
-
-}))
+router.post('/', authControlller.login);
 
 export default router;
